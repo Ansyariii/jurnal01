abb629f6ad1cb05aea05f04e8b3ec7c016440642
Ansyariii <farhanansari15.fa@gmail.com>
2023-09-28 14:09:14 +0700

    pembuatan nama

---
+++ jurnal01.java
@@ -0,0 +1,7 @@
+public class jurnal01{
+    public static void main(String[] args) {
+        System.out.println("Nama: Muhammad Ansyari Farhan");
+        System.out.println("NIM: 607062330027");
+     System.out.println("Kelas: D3IF-47-01" );
+    }
+}
